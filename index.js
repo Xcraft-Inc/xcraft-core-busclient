ec2e3c9218349daf22f715e15f9b70f78cb407ec
Mathieu Schroeter <schroeter@epsitec.ch>
2015-05-15 14:23:15 +0200

    Transform busClient from a singleton to a newable object.

--- index.js
+++ index.js
@@ -10,69 +10,77 @@ var busConfig = require ('xcraft-core-etc').load ('xcraft-core-bus');
 var xBus      = require ('xcraft-core-bus');
 var xUtils    = require ('xcraft-core-utils');
 
-var subSocket  = axon.socket ('sub');
-var pushSocket = axon.socket ('push');
 
-var eventsRegistry   = {};
-var commandsRegistry = {};
+function BusClient () {
+  var self = this;
 
-var token       = 'invalid';
-var orcName     = null;
-var autoconnect = false;
-var connected   = false;
+  self._subSocket  = axon.socket ('sub');
+  self._pushSocket = axon.socket ('push');
 
-var events  = require ('./lib/events.js') (subSocket, eventsRegistry);
-var command = require ('./lib/command.js') (pushSocket, eventsRegistry, events);
+  self._eventsRegistry   = {};
+  self._commandsRegistry = {};
 
-var autoConnectToken = '';
-/* broadcasted by server */
-subSocket.subscribe ('greathall::*');
+  self._token       = 'invalid';
+  self._orcName     = null;
+  self._autoconnect = false;
+  self._connected   = false;
 
-/* broadcasted by bus */
-subSocket.subscribe ('gameover');
+  self.subscriptions = self._subSocket;
 
-subSocket.on ('message', function (topic, msg) {
-  if (topic === 'gameover') {
-    xLog.info ('Game Over');
-    connected = false;
-    exports.stop ();
-    return;
-  }
+  var Events = require ('./lib/events.js');
+  self.events = new Events (self, self._subSocket);
 
-  if (autoconnect && topic === 'greathall::heartbeat') {
-    autoconnect = false;
-    xUtils.generateToken (function (err, generatedToken) {
-      if (err) {
-        xLog.err (err);
-        return;
-      }
-      autoConnectToken = generatedToken;
-      command.send ('autoconnect', autoConnectToken);
-    });
+  var Command = require ('./lib/command.js');
+  self.command = new Command (self, self._pushSocket);
 
-    return;
-  }
+  var autoConnectToken = '';
 
-  if (!eventsRegistry.hasOwnProperty (topic)) {
-    return;
-  }
+  self._subSocket.subscribe ('greathall::*'); /* broadcasted by server */
+  self._subSocket.subscribe ('gameover');     /* broadcasted by bus */
 
-  xLog.verb ('notification received: %s', topic);
-
-  if (topic === 'greathall::autoconnect.finished') {
-    if (!connected && msg.data.autoConnectToken === autoConnectToken) {
-      connected = true;
-      eventsRegistry[topic] (msg);
+  self._subSocket.on ('message', function (topic, msg) {
+    if (topic === 'gameover') {
+      xLog.info ('Game Over');
+      self._connected = false;
+      self.stop ();
+      return;
     }
-    return;
-  }
 
-  if (msg.token === token) {
-    eventsRegistry[topic] (msg);
-  } else {
-    xLog.verb ('invalid token, event discarded');
-  }
-});
+    if (self._autoconnect && topic === 'greathall::heartbeat') {
+      self._autoconnect = false;
+      xUtils.generateToken (function (err, generatedToken) {
+        if (err) {
+          xLog.err (err);
+          return;
+        }
+        autoConnectToken = generatedToken;
+        self.command.send ('autoconnect', autoConnectToken);
+      });
+
+      return;
+    }
+
+    if (!self._eventsRegistry.hasOwnProperty (topic)) {
+      return;
+    }
+
+    xLog.verb ('notification received: %s', topic);
+
+    if (topic === 'greathall::autoconnect.finished') {
+      if (!self._connected && msg.data.autoConnectToken === autoConnectToken) {
+        self._connected = true;
+        self._eventsRegistry[topic] (msg);
+      }
+      return;
+    }
+
+    if (msg.token === self._token) {
+      self._eventsRegistry[topic] (msg);
+    } else {
+      xLog.verb ('invalid token, event discarded');
+    }
+  });
+}
 
 /**
  * Connect the client to the buses.
@@ -84,17 +92,19 @@ subSocket.on ('message', function (topic, msg) {
  * @param {string} [busToken]
  * @param {Function(err)} callback
  */
-exports.connect = function (busToken, callback) {
+BusClient.prototype.connect = function (busToken, callback) {
+  var self = this;
+
   /* Save bus token for checking. */
   async.parallel ([
     function (callback) {
-      subSocket.on ('connect', function (err) {
+      self._subSocket.on ('connect', function (err) {
         xLog.verb ('Bus client subscribed to notifications bus');
         callback (err);
       });
     },
     function (callback) {
-      pushSocket.on ('connect', function (err) {
+      self._pushSocket.on ('connect', function (err) {
         xLog.verb ('Bus client ready to send on command bus');
         callback (err);
       });
@@ -102,31 +112,31 @@ exports.connect = function (busToken, callback) {
   ], function (err) {
     /* TODO: Explain auto-connect mecha */
     if (!busToken) {
-      eventsRegistry['greathall::autoconnect.finished'] = function (msg) {
-        token            = msg.data.token;
-        orcName          = msg.data.orcName;
-        commandsRegistry = msg.data.cmdRegistry;
+      self._eventsRegistry['greathall::autoconnect.finished'] = function (msg) {
+        self._token            = msg.data.token;
+        self._orcName          = msg.data.orcName;
+        self._commandsRegistry = msg.data.cmdRegistry;
 
-        xLog.info (orcName + ' is serving ' + token + ' Great Hall');
+        xLog.info (self._orcName + ' is serving ' + self._token + ' Great Hall');
 
-        if (orcName) {
-          subSocket.subscribe (orcName + '::*');
+        if (self._orcName) {
+          self._subSocket.subscribe (self._orcName + '::*');
         }
 
         callback (err);
       };
 
-      autoconnect = true;
+      self._autoconnect = true;
       /* Autoconnect is sent when the server is ready (heartbeat). */
     } else {
-      token = busToken;
-      xLog.verb ('Connected with token: ' + token);
+      self._token = busToken;
+      xLog.verb ('Connected with token: ' + self._token);
       callback (err);
     }
   });
 
-  subSocket.connect (parseInt (busConfig.notifierPort), busConfig.host);
-  pushSocket.connect (parseInt (busConfig.commanderPort), busConfig.host);
+  self._subSocket.connect (parseInt (busConfig.notifierPort), busConfig.host);
+  self._pushSocket.connect (parseInt (busConfig.commanderPort), busConfig.host);
 };
 
 /**
@@ -134,13 +144,15 @@ exports.connect = function (busToken, callback) {
  *
  * @param {Function(err)} callback
  */
-exports.stop = function (callback) {
+BusClient.prototype.stop = function (callback) {
+  var self = this;
+
   async.parallel ([
     function (callback) {
-      subSocket.on ('close', callback);
+      self._subSocket.on ('close', callback);
     },
     function (callback) {
-      pushSocket.on ('close', callback);
+      self._pushSocket.on ('close', callback);
     }
   ], function (err) {
     xLog.verb ('Stopped');
@@ -150,22 +162,22 @@ exports.stop = function (callback) {
   });
 
   xLog.verb ('Stopping...');
-  subSocket.close ();
-  pushSocket.close ();
+  self._subSocket.close ();
+  self._pushSocket.close ();
 };
 
-exports.getToken = function () {
-  return token;
+BusClient.prototype.getToken = function () {
+  return this._token;
 };
 
-exports.getOrcName = function () {
-  return orcName;
+BusClient.prototype.getOrcName = function () {
+  return this._orcName;
 };
 
-exports.getStateWhich = function () {
+BusClient.prototype.getStateWhich = function () {
   /* Client side */
-  if (orcName) {
-    return orcName;
+  if (this._orcName) {
+    return this._orcName;
   }
 
   /* Server side */
@@ -178,15 +190,24 @@ exports.getStateWhich = function () {
   return null;
 };
 
-exports.getCommandsRegistry = function () {
-  return commandsRegistry;
+BusClient.prototype.getEventsRegistry = function () {
+  return this._eventsRegistry;
 };
 
-exports.isConnected = function () {
-  return connected;
+BusClient.prototype.getCommandsRegistry = function () {
+  return this._commandsRegistry;
 };
 
-exports.subscriptions = subSocket;
+BusClient.prototype.isConnected = function () {
+  return this._connected;
+};
 
-exports.events  = events;
-exports.command = command;
+BusClient.prototype.topicModifier = function (topic) {
+  if (/.*::.*/.test (topic)) {
+    return topic;
+  }
+
+  return this.getStateWhich () + '::' + topic;
+};
+
+module.exports = new BusClient ();
