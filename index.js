d76afa87fd8cc994e50a0548f12ddaaac332eb70
Mathieu Schroeter <schroeter@epsitec.ch>
2017-05-01 16:52:57 +0200

    Rewrite the client with an ES6 class

--- index.js
+++ index.js
@@ -14,247 +14,251 @@ const Resp = require ('./lib/resp.js');
 
 var globalBusClient = null;
 
-function BusClient (busConfig) {
-  EventEmitter.call (this);
+class BusClient extends EventEmitter {
+  constructor (busConfig) {
+    super ();
 
-  var self = this;
+    var self = this;
 
-  self._busConfig = busConfig
-    ? busConfig
-    : require ('xcraft-core-etc') ().load ('xcraft-core-bus');
+    self._busConfig = busConfig
+      ? busConfig
+      : require ('xcraft-core-etc') ().load ('xcraft-core-bus');
 
-  self._subSocket = axon.socket ('sub');
-  self._pushSocket = axon.socket ('push');
+    self._subSocket = axon.socket ('sub');
+    self._pushSocket = axon.socket ('push');
 
-  self._eventsRegistry = {};
-  self._commandsRegistry = {};
+    self._eventsRegistry = {};
+    self._commandsRegistry = {};
 
-  self._token = 'invalid';
-  self._orcName = null;
-  self._autoconnect = false;
-  self._connected = false;
+    self._token = 'invalid';
+    self._orcName = null;
+    self._autoconnect = false;
+    self._connected = false;
 
-  var Events = require ('./lib/events.js');
-  self.events = new Events (self, self._subSocket);
+    var Events = require ('./lib/events.js');
+    self.events = new Events (self, self._subSocket);
 
-  var Command = require ('./lib/command.js');
-  self.command = new Command (self, self._pushSocket);
+    var Command = require ('./lib/command.js');
+    self.command = new Command (self, self._pushSocket);
 
-  var autoConnectToken = '';
+    var autoConnectToken = '';
 
-  self._subSocket.subscribe ('greathall::*'); /* broadcasted by server */
-  self._subSocket.subscribe ('gameover'); /* broadcasted by bus */
+    self._subSocket.subscribe ('greathall::*'); /* broadcasted by server */
+    self._subSocket.subscribe ('gameover'); /* broadcasted by bus */
 
-  self._subSocket.on ('message', function (topic, msg) {
-    if (topic === 'gameover') {
-      xLog.info ('Game Over');
-      self._connected = false;
-      self.stop ();
-      return;
-    }
-
-    if (topic === 'greathall::bus.commands.registry') {
-      self._commandsRegistry = msg.data;
-      self.emit ('commands.registry');
-      return;
-    }
-
-    if (self._autoconnect && topic === 'greathall::heartbeat') {
-      self._autoconnect = false;
-      xUtils.crypto.genToken (function (err, generatedToken) {
-        if (err) {
-          xLog.err (err);
-          return;
-        }
-        autoConnectToken = generatedToken;
-        self._subSocket.subscribe (autoConnectToken + '::autoconnect.finished');
-        self.command.send ('autoconnect', autoConnectToken);
-      });
-
-      return;
-    }
-
-    if (
-      !self._connected &&
-      topic === autoConnectToken + '::autoconnect.finished'
-    ) {
-      self._connected = true;
-      self._subSocket.unsubscribe (autoConnectToken + '::autoconnect.finished');
-      self._eventsRegistry['autoconnect.finished'] (msg);
-      delete self._eventsRegistry['autoconnect.finished'];
-      return;
-    }
-
-    if (!self._eventsRegistry.hasOwnProperty (topic)) {
-      return;
-    }
-
-    xLog.verb ('notification received: %s', topic);
-
-    if (msg.token === self._token) {
-      self._eventsRegistry[topic] (msg);
-    } else {
-      xLog.verb ('invalid token, event discarded');
-    }
-  });
-}
-
-util.inherits (BusClient, EventEmitter);
-
-/**
- * Connect the client to the buses.
- *
- * If the bus is not known, the argument can be null, then the client tries
- * to autoconnect to the server. It's a trivial mechanism, there is no
- * support for user authentication.
- *
- * @param {string} [busToken]
- * @param {function(err)} callback
- */
-BusClient.prototype.connect = function (busToken, callback) {
-  var self = this;
-
-  /* Save bus token for checking. */
-  async.parallel (
-    [
-      function (callback) {
-        self._subSocket
-          .on ('connect', function () {
-            xLog.verb ('Bus client subscribed to notifications bus');
-            callback ();
-          })
-          .on ('error', callback);
-      },
-      function (callback) {
-        self._pushSocket
-          .on ('connect', function () {
-            xLog.verb ('Bus client ready to send on command bus');
-            callback ();
-          })
-          .on ('error', callback);
-      },
-    ],
-    function (err) {
-      if (err) {
-        callback (err);
+    self._subSocket.on ('message', function (topic, msg) {
+      if (topic === 'gameover') {
+        xLog.info ('Game Over');
+        self._connected = false;
+        self.stop ();
         return;
       }
 
-      /* TODO: Explain auto-connect mecha */
-      if (!busToken) {
-        self._eventsRegistry['autoconnect.finished'] = function (msg) {
-          self._token = msg.data.token;
-          self._orcName = msg.data.orcName;
-          self._commandsRegistry = msg.data.cmdRegistry;
+      if (topic === 'greathall::bus.commands.registry') {
+        self._commandsRegistry = msg.data;
+        self.emit ('commands.registry');
+        return;
+      }
 
-          xLog.info (
-            self._orcName + ' is serving ' + self._token + ' Great Hall'
-          );
-
-          if (self._orcName) {
-            self._subSocket.subscribe (self._orcName + '::*');
+      if (self._autoconnect && topic === 'greathall::heartbeat') {
+        self._autoconnect = false;
+        xUtils.crypto.genToken (function (err, generatedToken) {
+          if (err) {
+            xLog.err (err);
+            return;
           }
+          autoConnectToken = generatedToken;
+          self._subSocket.subscribe (
+            autoConnectToken + '::autoconnect.finished'
+          );
+          self.command.send ('autoconnect', autoConnectToken);
+        });
 
-          callback (err);
-        };
+        return;
+      }
 
-        self._autoconnect = true;
-        /* Autoconnect is sent when the server is ready (heartbeat). */
-      } else {
+      if (
+        !self._connected &&
+        topic === autoConnectToken + '::autoconnect.finished'
+      ) {
         self._connected = true;
-        self._token = busToken;
-        xLog.verb ('Connected with token: ' + self._token);
-        callback (err);
+        self._subSocket.unsubscribe (
+          autoConnectToken + '::autoconnect.finished'
+        );
+        self._eventsRegistry['autoconnect.finished'] (msg);
+        delete self._eventsRegistry['autoconnect.finished'];
+        return;
       }
-    }
-  );
 
-  self._subSocket.connect (
-    parseInt (self._busConfig.notifierPort),
-    self._busConfig.host
-  );
-  self._pushSocket.connect (
-    parseInt (self._busConfig.commanderPort),
-    self._busConfig.host
-  );
-};
-
-/**
- * Close the connections on the buses.
- *
- * @param {function(err)} callback
- */
-BusClient.prototype.stop = function (callback) {
-  var self = this;
-
-  async.parallel (
-    [
-      function (callback) {
-        self._subSocket.on ('close', callback);
-      },
-      function (callback) {
-        self._pushSocket.on ('close', callback);
-      },
-    ],
-    function (err) {
-      xLog.verb ('Stopped');
-      if (callback) {
-        callback (err);
+      if (!self._eventsRegistry.hasOwnProperty (topic)) {
+        return;
       }
-    }
-  );
 
-  xLog.verb ('Stopping...');
-  self._connected = false;
-  self._subSocket.close ();
-  self._pushSocket.close ();
-};
+      xLog.verb ('notification received: %s', topic);
 
-/**
- * Return a new empty message for the commands.
- *
- * The \p isNested attribute is set when the command is called from the server
- * side but with an orc name.
- *
- * @return {object} the new message.
- */
-BusClient.prototype.newMessage = function (which) {
-  return {
-    token: this.getToken (),
-    orcName: which,
-    timestamp: new Date ().toISOString (),
-    data: {},
-    isNested: !!(this.isServerSide () && which && which !== 'greathall'),
-  };
-};
+      if (msg.token === self._token) {
+        self._eventsRegistry[topic] (msg);
+      } else {
+        xLog.verb ('invalid token, event discarded');
+      }
+    });
+  }
 
-BusClient.prototype.isServerSide = function () {
-  return !this._orcName;
-};
+  /**
+   * Connect the client to the buses.
+   *
+   * If the bus is not known, the argument can be null, then the client tries
+   * to autoconnect to the server. It's a trivial mechanism, there is no
+   * support for user authentication.
+   *
+   * @param {string} [busToken]
+   * @param {function(err)} callback
+   */
+  connect (busToken, callback) {
+    var self = this;
 
-BusClient.prototype.getToken = function () {
-  return this._token;
-};
+    /* Save bus token for checking. */
+    async.parallel (
+      [
+        function (callback) {
+          self._subSocket
+            .on ('connect', function () {
+              xLog.verb ('Bus client subscribed to notifications bus');
+              callback ();
+            })
+            .on ('error', callback);
+        },
+        function (callback) {
+          self._pushSocket
+            .on ('connect', function () {
+              xLog.verb ('Bus client ready to send on command bus');
+              callback ();
+            })
+            .on ('error', callback);
+        },
+      ],
+      function (err) {
+        if (err) {
+          callback (err);
+          return;
+        }
 
-BusClient.prototype.getOrcName = function () {
-  return this._orcName;
-};
+        /* TODO: Explain auto-connect mecha */
+        if (!busToken) {
+          self._eventsRegistry['autoconnect.finished'] = function (msg) {
+            self._token = msg.data.token;
+            self._orcName = msg.data.orcName;
+            self._commandsRegistry = msg.data.cmdRegistry;
 
-BusClient.prototype.getEventsRegistry = function () {
-  return this._eventsRegistry;
-};
+            xLog.info (
+              self._orcName + ' is serving ' + self._token + ' Great Hall'
+            );
 
-BusClient.prototype.getCommandsRegistry = function () {
-  return this._commandsRegistry;
-};
+            if (self._orcName) {
+              self._subSocket.subscribe (self._orcName + '::*');
+            }
 
-BusClient.prototype.isConnected = function () {
-  return this._connected;
-};
+            callback (err);
+          };
 
-BusClient.prototype.newResponse = function () {
-  return exports.newResponse.apply (this, arguments);
-};
+          self._autoconnect = true;
+          /* Autoconnect is sent when the server is ready (heartbeat). */
+        } else {
+          self._connected = true;
+          self._token = busToken;
+          xLog.verb ('Connected with token: ' + self._token);
+          callback (err);
+        }
+      }
+    );
+
+    self._subSocket.connect (
+      parseInt (self._busConfig.notifierPort),
+      self._busConfig.host
+    );
+    self._pushSocket.connect (
+      parseInt (self._busConfig.commanderPort),
+      self._busConfig.host
+    );
+  }
+
+  /**
+   * Close the connections on the buses.
+   *
+   * @param {function(err)} callback
+   */
+  stop (callback) {
+    var self = this;
+
+    async.parallel (
+      [
+        function (callback) {
+          self._subSocket.on ('close', callback);
+        },
+        function (callback) {
+          self._pushSocket.on ('close', callback);
+        },
+      ],
+      function (err) {
+        xLog.verb ('Stopped');
+        if (callback) {
+          callback (err);
+        }
+      }
+    );
+
+    xLog.verb ('Stopping...');
+    self._connected = false;
+    self._subSocket.close ();
+    self._pushSocket.close ();
+  }
+
+  /**
+   * Return a new empty message for the commands.
+   *
+   * The \p isNested attribute is set when the command is called from the server
+   * side but with an orc name.
+   *
+   * @return {object} the new message.
+   */
+  newMessage (which) {
+    return {
+      token: this.getToken (),
+      orcName: which,
+      timestamp: new Date ().toISOString (),
+      data: {},
+      isNested: !!(this.isServerSide () && which && which !== 'greathall'),
+    };
+  }
+
+  isServerSide () {
+    return !this._orcName;
+  }
+
+  getToken () {
+    return this._token;
+  }
+
+  getOrcName () {
+    return this._orcName;
+  }
+
+  getEventsRegistry () {
+    return this._eventsRegistry;
+  }
+
+  getCommandsRegistry () {
+    return this._commandsRegistry;
+  }
+
+  isConnected () {
+    return this._connected;
+  }
+
+  newResponse () {
+    return exports.newResponse.apply (this, arguments);
+  }
+}
 
 exports.newResponse = function (moduleName, orcName) {
   let self = null;
